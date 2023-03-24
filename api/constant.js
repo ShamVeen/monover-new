6797dc7a09fee3b2016305e96fd2cff9116e429f
ShamVeen <118723873+ShamVeen@users.noreply.github.com>
2023-03-24 12:47:00 +0530

    Delete api directory

--- api/constant.js
+++
@@ -1,9 +0,0 @@
-export const ALL_API_CONSTANTS = {
-    review: {
-        allDocuments: '/api/alldocuments/',
-        suggestedDocuments: '/api/suggested/document/',
-        addDocuments: "/api/document/",
-        allFolders: "/api/cu-subproject/",
-        allBatches: "/api/batch-create",
-    }
-}
